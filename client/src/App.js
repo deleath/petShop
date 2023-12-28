f5b1b863179c6b4fe31d52473fc8bf6b829b068c
deleath <101787610+deleath@users.noreply.github.com>
2023-12-28 18:03:50 +0300

    Fix error handling

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ const App = observer(() => {
         check().then(data => {
             user.setUser(true)
             user.setIsAuth(true)
-        }).finally(() => setLoading(false))
+        }).catch(() => setLoading(false))
     }, [])
 
     if (loading) {
